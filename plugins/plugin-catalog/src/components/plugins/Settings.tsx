2f6b2fb5d5f2113ae0afb826c46654835c88161a
yolossn <sannagaraj@microsoft.com>
2026-08-13 22:06:53 +0530

    plugins: Add copy of plugin-catalog from headlamp-k8s/plugins
    Signed-off-by: yolossn <sannagaraj@microsoft.com>

---
+++ plugins/plugin-catalog/src/components/plugins/Settings.tsx
@@ -0,0 +1,46 @@
+import { useTranslation } from '@kinvolk/headlamp-plugin/lib';
+import { NameValueTable } from '@kinvolk/headlamp-plugin/lib/components/common';
+import Box from '@mui/material/Box';
+import Switch from '@mui/material/Switch';
+
+export function Settings(props) {
+  const { data, onDataChange } = props;
+  const { t } = useTranslation();
+
+  const settingsRows = [
+    {
+      name: t('Display only Official Plugins'),
+      value: (
+        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+          <Switch
+            checked={data?.displayOnlyOfficialPlugins ?? true}
+            inputProps={{ 'aria-label': t('Display only Official Plugins') }}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              onDataChange({ ...data, displayOnlyOfficialPlugins: event.target.checked })
+            }
+          />
+        </Box>
+      ),
+    },
+    {
+      name: t('Display only Verified Plugins'),
+      value: (
+        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+          <Switch
+            checked={data?.displayOnlyVerifiedPlugins ?? true}
+            inputProps={{ 'aria-label': t('Display only Verified Plugins') }}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              onDataChange({ ...data, displayOnlyVerifiedPlugins: event.target.checked })
+            }
+          />
+        </Box>
+      ),
+    },
+  ];
+
+  return (
+    <Box width={'100%'}>
+      <NameValueTable rows={settingsRows} />
+    </Box>
+  );
+}
